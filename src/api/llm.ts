f88d71e37d3dab51652d2402d758c3dc1a8cf887
Jimmy Lau <jlau@StoneCastlePartners.com>
2023-11-21 17:02:01 -0500

    Add list support

--- src/api/llm.ts
+++ src/api/llm.ts
@@ -3,7 +3,9 @@ import { code } from "../stores/model";
 export async function callLLM(model: string, prompt: string) {
   const URL = `https://${get(code)}-151-205-163-6.ngrok-free.app/api/generate`;
   const prepend = "Respond to the following with less than 300 characters. ";
-  const parsedPrompt = prepend + prompt.trim();
+  let parsedPrompt = prepend + prompt.trim().replaceAll(`"`, `'`);
+  parsedPrompt = parsedPrompt.replaceAll("\\", "\\\\");
+  console.log("pp", parsedPrompt);
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "text/plain");
 
--- src/lib/ChatBubble.svelte
+++ src/lib/ChatBubble.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+  import { code } from "../stores/model";
+
   export let sender = "";
   export let text = "";
+
+  const ex = /^(\d+)([.][ ])|([*][ ])|([-][ ])/gm;
+
+  const regex = new RegExp(ex, "gm");
 
   $: displayText = [text];
 
   $: {
     console.log("TEXT: ", text, displayText);
-    if (text.includes("``")) {
+    if (text.includes("```")) {
       displayText = text.split("```");
     }
   }
@@ -21,14 +27,36 @@
 {:else}
   {#each displayText as t, i}
     {#if i % 2 == 0}
-      <div class="chat chat-end">
-        <div class="chat-bubble chat-bubble-warning">
-          {t.length ? t : "processing ..."}
+      {#if !t.length}
+        <!-- Message for empty text -->
+        <div class="chat chat-end">
+          <div class="chat-bubble chat-bubble-warning">processing ...</div>
         </div>
-      </div>
+      {:else if t.includes("\n") && t.match(regex)}
+        <!-- Handle line break/lists -->
+        <div class="chat chat-end">
+          <div class="chat-bubble chat-bubble-warning">
+            <ul class="menu rounded-box">
+              {#each t.split("\n") as v, i}
+                <li>{v}</li>
+              {/each}
+            </ul>
+          </div>
+        </div>
+      {:else}
+        <!-- Show standard text if not empty or list -->
+        <div class="chat chat-end">
+          <div class="chat-bubble chat-bubble-warning">
+            {t}
+          </div>
+        </div>
+      {/if}
     {:else}
-      <div class="mockup-code">
-        <pre>{t}</pre>
+      <!-- Must be code, in this case embed. -->
+      <div class="chat chat-end">
+        <div class=" mockup-code float-right flex-col">
+          <pre>{t}</pre>
+        </div>
       </div>
     {/if}
   {/each}
